440edc9f85634a2440fb13a6147c1607a721f064
esengul <esenko2222@gmail.com>
2026-02-17 20:28:46 +0600

    add mobail hedar

---
+++ main.js
@@ -0,0 +1,22 @@
+// let burger = document.querySelector(".header__burger");
+// let menu = document.querySelector('.header__right');
+
+// const clickBurger = ()=>{
+//     menu.classList.toggle('active');
+// }
+// let burger = document.querySelector(".header__burger");
+// let menu = document.querySelector(".header__right");
+
+// const clickBurger = () => {
+//   menu.classList.toggle("active");
+// };
+
+let form = document.querySelector(".form");
+
+const clickPopup = () => {
+  form.classList.toggle("active");
+};
+
+form.addEventListener("click",  (e) =>{
+  e.preventDefault();
+});
