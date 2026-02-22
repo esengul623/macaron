e996d0d0d6ad5c028def96b4ee5762e3f3202cc0
esengul623 <esenko2222@gmail.com>
2026-02-22 06:52:32 +0600

    Update main.js

--- main.js
+++ main.js
@@ -4,12 +4,12 @@
 // const clickBurger = ()=>{
 //     menu.classList.toggle('active');
 // }
-// let burger = document.querySelector(".header__burger");
-// let menu = document.querySelector(".header__right");
+let burger = document.querySelector(".header__burger");
+let menu = document.querySelector(".header__right");
 
-// const clickBurger = () => {
-//   menu.classList.toggle("active");
-// };
+const clickBurger = () => {
+  menu.classList.toggle("active");
+};
 
 let form = document.querySelector(".form");
 
